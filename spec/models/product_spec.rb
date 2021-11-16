6ad1885b63f5efb1a4b1bd28b9ad126be155eded
Jordan Cronier <jcronier777@gmail.com>
2021-11-16 05:17:57 +0000

    Test model Product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    # validation tests/examples here
+    it "saves with valid attributes" do
+      @category = Category.find(1)
+      @product = Product.new(name: "product", price: 100, quantity: 2, category: @category)
+      @product.save!
+      expect(@product).to be_present
+    end
+    it "does not save without a name" do
+      @category = Category.find(1)
+      @product = Product.new(name: nil, price: 100, quantity: 2, category: @category)
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:name]).to eq(["can't be blank"])
+    end
+    it "does not save without a price" do
+      @category = Category.find(1)
+      @product = Product.new(name: "product", price: nil, quantity: 2, category: @category)
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:price]).to eq(["is not a number", "can't be blank"])
+    end
+    it "does not save without a quantity" do
+      @category = Category.find(1)
+      @product = Product.new(name: "product", price: 100, quantity: nil, category: @category)
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:quantity]).to eq(["can't be blank"])
+    end
+    it "does not save without a category" do
+      @category = Category.find(1)
+      @product = Product.new(name: "product", price: 100, quantity: 2, category: nil)
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:category]).to eq(["can't be blank"])
+    end
+  end
+end
